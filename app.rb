e5574e373e2e4ce83440024871ab6ce4c94c1104
dewslyse <dewslyse@gmail.com>
2022-06-17 22:03:51 +0000

    Add method for creating person

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require_relative 'book'
+require_relative 'student'
+require_relative 'teacher'
 
 class App
   attr_reader :books
 
   def initialize
     @all_books = []
+    @all_persons = []
   end
 
   def list_books
@@ -12,6 +15,31 @@ class App
     @all_books.each do |book|
       puts "Title: \"#{book.title}\", Author: #{book.author}"
     end
+  end
+
+  def create_person
+    puts 'Add a person'
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    type_of_person = gets.chomp.to_i
+
+    create_person while type_of_person != 1 && type_of_person != 2
+
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+
+    if type_of_person == 1
+      print 'Has parent permission? [Y/N]: '
+      permission = gets.chomp.downcase
+      parent_permission = permission == 'y'
+      @all_persons << Student.new(age, name, parent_permission)
+    else
+      print 'Specialization: '
+      specialization = gets.chomp
+      @all_persons << Teacher.new(specialization, age, name)
+    end
+    puts 'Person created successfully'
   end
 
   def create_book
