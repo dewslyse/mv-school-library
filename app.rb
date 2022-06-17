dca5e3e3f72ffbb6d3fc6b3a2c668d7b9bae1085
dewslyse <dewslyse@gmail.com>
2022-06-17 17:22:50 +0000

    Add create book method

--- app.rb
+++ app.rb
@@ -1,2 +1,17 @@
-class App  
+require_relative 'book'
+
+class App
+  def initialize
+    @books = []
+  end
+
+  def create_book
+    puts 'Create a book'
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    puts 'Book created successfully'
+    @books << Book.new(title, author)
+  end
 end
