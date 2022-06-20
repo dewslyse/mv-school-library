5e63dbacfcbb69eb3f2ca0741d2e14b42a16228a
dewslyse <dewslyse@gmail.com>
2022-06-20 21:49:59 +0000

    Clean up file

--- main.rb
+++ main.rb
@@ -2,10 +2,8 @@ require_relative 'app'
 require_relative 'menu'
 
 def main
-  puts
-  puts 'Welcome to the School Library App!'
+  puts "\nWelcome to the School Library App!\n\n"
   menu
-  puts 'Thank you for using this app ;)'
 end
 
 main
