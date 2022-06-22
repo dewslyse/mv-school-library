0390c27c875a5709a66f0d5c8b8095918b188b33
dewslyse <dewslyse@gmail.com>
2022-06-22 19:53:02 +0000

    Test nameable class

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../nameable'
+
+describe Nameable do
+  context 'Testing Nameable class' do
+    before :each do
+      @nameable = Nameable.new
+    end
+
+    it 'raises error' do
+      expect{@nameable.correct_name}.to raise_error(NotImplementedError)
+    end
+  end
+end
