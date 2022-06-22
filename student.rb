fb354849d20bc4d5e3e9c971d478168778d4d25e
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-22 13:33:35 -0600

    Made tests for classroom class

--- student.rb
+++ student.rb
@@ -6,6 +6,7 @@ class Student < Person
   def initialize(age:, name: 'Unknown', classroom: nil, parent_permission: true, id: rand(1..1000))
     super(age: age, name: name, parent_permission: parent_permission, id: id)
     @classroom = classroom
+    classroom.students << self if classroom
   end
 
   def play_hooky
